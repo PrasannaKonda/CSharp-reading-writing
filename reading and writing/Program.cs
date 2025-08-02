dfd5f7238e44ae8622098daf61944bf05e74d2f6
PrasannaKonda <kondalakshmiprasanna1804@gmail.com>
2025-08-02 21:14:03 +0530

    convert non-nullable value to nullable value

--- reading and writing/Program.cs
+++ reading and writing/Program.cs
@@ -4,10 +4,7 @@ class Program
 {
     static void Main()
     {
-        int Numerator = 10;
-        int Denominator = 2;
-        int Result = Numerator / Denominator;
-        Console.WriteLine("Result {0}", Result);
-        Console.ReadLine();
+        //int a = null here int is a non-nullable type to make it nullable you can use the ? operator
+        int? i = null
     }
 }
