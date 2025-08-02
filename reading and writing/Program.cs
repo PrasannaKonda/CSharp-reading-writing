f3a664e3e0b48b2f25026a702eb37a92d1b3a7f8
PrasannaKonda <kondalakshmiprasanna1804@gmail.com>
2025-08-02 15:05:43 +0530

    arithmetic operator /

--- reading and writing/Program.cs
+++ reading and writing/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int Numerator = 10;
+        int Denominator = 2;
+        int Result = Numerator / Denominator;
+        Console.WriteLine("Result {0}", Result);
+        Console.ReadLine();
+    }
+}
