6303359cb1f71c89bfc366ee9bf1b5666274d350
PrasannaKonda <kondalakshmiprasanna1804@gmail.com>
2025-08-02 21:43:36 +0530

    Example for non nullable value

--- reading and writing/Program.cs
+++ reading and writing/Program.cs
@@ -4,7 +4,20 @@ class Program
 {
     static void Main()
     {
-        //int a = null here int is a non-nullable type to make it nullable you can use the ? operator
-        int? i = null
+        // here trying to explain the scenario of nullable value type i.e, user has a chance to not to specify he is major or not
+        bool? AreYouMajor = null;  //here bool is a value type
+
+        if (AreYouMajor == true)
+        {
+            Console.WriteLine("User is major.");
+        }
+        else if (AreYouMajor == false)
+        {
+            Console.WriteLine("User is not major.");
+        }
+        else
+        {
+            Console.WriteLine("User not Answer the Question.");
+        }
     }
 }
