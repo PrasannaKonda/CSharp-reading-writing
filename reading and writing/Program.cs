3d60d71815552b3e8d296667e25c51ef78c79e99
PrasannaKonda <kondalakshmiprasanna1804@gmail.com>
2025-08-04 17:16:58 +0530

    goto label in switch statement

--- reading and writing/Program.cs
+++ reading and writing/Program.cs
@@ -3,20 +3,45 @@
 class Program {
     static void Main()
     {
-        Console.WriteLine("Please enter a number:");
-        int UserNumber = int.Parse(Console.ReadLine());
-        //Joining multiple cases in a switch statement
-        switch (UserNumber)
+        int TotalCoffeeCost = 0;
+        Start:
+        Console.WriteLine("Please Select Your Coffee : 1 - Small, 2 - Medium, 3 - Large");
+        int UserChoice = int.Parse(Console.ReadLine());
+
+       
+        switch(UserChoice)
         {
-            case 10:
-            case 20:
-            case 30:
-                Console.WriteLine("Your Number is {0}", UserNumber);
+            case 1:
+                TotalCoffeeCost += 1;
+                break;
+            case 2:
+                TotalCoffeeCost += 2;
+                break;
+            case 3:
+                TotalCoffeeCost += 3;
                 break;
             default:
-                Console.WriteLine("your number is not 10, 20 or 30");
-                break;
+                Console.WriteLine("User choice {0} is invalid", UserChoice);
+                goto Start;
         }
+
+        Decide:
+        Console.WriteLine("Do you want to buy another coffee - Yes or No?");
+        string UserDecision = Console.ReadLine();
+
+        switch (UserDecision.ToUpper())
+        {
+            case "YES":
+                goto Start;
+            case "NO":
+                break;
+            default:
+                Console.WriteLine("Your choice {0} is invalid. Please try again", UserDecision);
+                goto Decide;
+        }
+
+        Console.WriteLine("Thank you for shopping with us!");
+        Console.WriteLine("Bill Amount = ${0}", TotalCoffeeCost);
 
     }
 }
