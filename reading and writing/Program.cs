7e701e2b7e8c825361179278b142ca308fe5e843
PrasannaKonda <kondalakshmiprasanna1804@gmail.com>
2025-08-04 10:18:36 +0530

    switch statement

--- reading and writing/Program.cs
+++ reading and writing/Program.cs
@@ -3,12 +3,21 @@
 class Program {
     static void Main()
     {
-        int[] EvenNumbers = new int[3];
+        Console.WriteLine("Please enter a number:");
+        int UserNumber = int.Parse(Console.ReadLine());
+        //Joining multiple cases in a switch statement
+        switch (UserNumber)
+        {
+            case 10:
+            case 20:
+            case 30:
+                Console.WriteLine("Your Number is {0}", UserNumber);
+                break;
+            default:
+                Console.WriteLine("your number is not 10, 20 or 30");
+                break;
+        }
 
-        EvenNumbers[0] = 0;
-        EvenNumbers[1] = 2;
-        EvenNumbers[2] = 4;
-
-        Console.WriteLine(EvenNumbers[2]);
     }
 }
+
