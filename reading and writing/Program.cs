0103ad901dd974541deadd65ac361dc9c6f26c05
PrasannaKonda <kondalakshmiprasanna1804@gmail.com>
2025-08-05 16:27:37 +0530

    do while loop

--- reading and writing/Program.cs
+++ reading and writing/Program.cs
@@ -4,16 +4,31 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Please enter your target?");
-        int UserTarget = int.Parse(Console.ReadLine());
-
-        int Start = 0;
-
-        while(Start <= UserTarget)
+        string UserChoice = string.Empty;
+        do
         {
-            //Console.WriteLine(Start);   //it prints values in a new line
-            Console.Write(Start+" ");   //it's print the values in same line with a space
-            Start += 2;
-        }
+            Console.WriteLine("Please enter your target?");
+            int UserTarget = int.Parse(Console.ReadLine());
+
+            int Start = 0;
+
+            while (Start <= UserTarget)
+            {
+                //Console.WriteLine(Start);   //it prints values in a new line
+                Console.Write(Start + " ");   //it's print the values in same line with a space
+                Start += 2;
+            }
+
+
+            do
+            {
+                Console.WriteLine("Do you want to continue - Yes or No?");
+                UserChoice = Console.ReadLine().ToUpper();
+                if (UserChoice != "YES" && UserChoice != "NO")
+                {
+                    Console.WriteLine("Invalid Choice. Please say Yes or No.");
+                }
+            } while (UserChoice != "YES" && UserChoice != "NO");
+        } while (UserChoice == "YES");
     }
 }
