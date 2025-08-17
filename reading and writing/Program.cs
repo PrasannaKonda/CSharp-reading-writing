e9068bc5b31f03ed8c0922d9a08ffee57690e35c
PrasannaKonda <kondalakshmiprasanna1804@gmail.com>
2025-08-17 12:31:37 +0530

    Continue Key word

--- reading and writing/Program.cs
+++ reading and writing/Program.cs
@@ -1,37 +1,14 @@
 ﻿using System;
 
-class Program {
-    static void Main() {
-        int[] Numbers = new int[3];
-
-        Numbers[0] = 101;
-        Numbers[1] = 102;
-        Numbers[2] = 103;
-
-        foreach(int k in Numbers)
+class Program
+{
+    static void Main()
+    {
+        for (int i=1; i<=20; i++)
         {
-            Console.WriteLine(k);
+            if (i % 2 == 1)
+                continue;
+            Console.WriteLine(i);
         }
-
-        //int k = 0;
-
-        //do
-        //{
-        //    Console.WriteLine(Numbers[k]);
-        //    k++;
-        //}
-        //while (k < Numbers.Length);
-
-        //for (int j=0; j < Numbers.Length; j++)
-        //{
-        //    Console.WriteLine(Numbers[j]);
-        //}
-
-        //int i = 0;
-        //while (i < Numbers.Length)
-        //{
-        //    Console.WriteLine(Numbers[i]);
-        //    i++;
-        //}
     }
 }
