41ca18ec552377f9f89cebdece34adbd5bba42b6
PrasannaKonda <kondalakshmiprasanna1804@gmail.com>
2025-08-17 13:00:00 +0530

    Instance Method

--- reading and writing/Program.cs
+++ reading and writing/Program.cs
@@ -4,13 +4,25 @@ class Program
 {
     static void Main()
     {
-        Program.EvenNumbers();
+        // Calling Static method
+        Program.EvenNumbers(30);
+        //Calling Instance method
+        Program p = new Program();
+        int Sum = p.Add(5, 10);
+        Console.WriteLine(Sum);
     }
 
-    public static void EvenNumbers()
+    // Instance method to add two numbers
+    public int Add(int fn, int sn)
+    {
+        return fn + sn;
+    }
+
+    // Static method to print even numbers up to a target number
+    public static void EvenNumbers(int Target)
     {
         int Start = 0;
-        while (Start <= 20)
+        while (Start <= Target)
         { 
             Console.WriteLine(Start);
             Start += 2;
