372af75ba79963a6568e4d16a00ef92130f04a40
PrasannaKonda <kondalakshmiprasanna1804@gmail.com>
2025-08-29 14:52:03 +0530

    Abstraction

--- reading and writing/Program.cs
+++ reading and writing/Program.cs
@@ -1,39 +1,21 @@
-﻿interface I1
+﻿using System;
+
+public abstract class Customer
 {
-    void InterfaceMethod();
+    public abstract void Print();
 }
 
-interface I2
+public class  Program : Customer
 {
-    void InterfaceMethod();
-}
-
-public class  Program : I1, I2
-{
-    public void InterfaceMethod()
+    public override void Print()
     {
-        Console.WriteLine("I1 Interface Method");
-    }
-    void I2.InterfaceMethod()
-    {
-        Console.WriteLine("I2 Interface Method");
+        Console.WriteLine("Print Method");
     }
     public static void Main()
     {
-        //Explicit interface implementation
         //Program p = new Program();
-        //((I1)p).InterfaceMethod();
-        //((I2)p).InterfaceMethod();
-
-        //I1 i1 = new Program();
-        //I2 i2 = new Program();
-        //i1.InterfaceMethod();
-        //i2.InterfaceMethod();
-
-        //Default and Explicit interface implementation
-        Program p = new Program();
-        p.InterfaceMethod();
-        I2 i2 = new Program();
-        i2.InterfaceMethod();
+        //p.Print();
+        Customer c = new Program();
+        c.Print();
     }
 }
