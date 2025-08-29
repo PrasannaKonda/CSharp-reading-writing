83799b5af06053dda412d30024932066ae3e5e47
PrasannaKonda <kondalakshmiprasanna1804@gmail.com>
2025-08-29 16:50:51 +0530

    Delegates

--- reading and writing/Program.cs
+++ reading and writing/Program.cs
@@ -1,54 +1,17 @@
 ﻿using System;
 
-interface IA
-{
-    void AMethod();
-}
-
-class A : IA
-{
-    public void AMethod()
-    {
-        Console.WriteLine("A");
-    }
-}
-
-interface IB
-{
-    void BMethod();
-}
-
- class B : IB
-{
-    public void BMethod()
-    {
-        Console.WriteLine("B");
-    }
-}
-
-class AB : IA, IB
-{
-    A a = new A();
-    B b = new B();
-
-    public void AMethod()
-    {
-        a.AMethod();
-    }
-
-    public void BMethod()
-    {
-        b.BMethod();
-    }
-}
-
-
-public class  Program
+public delegate void HelloFunctionDelegate(string Message);
+public class Program
 {
     public static void Main()
     {
-        AB ab = new AB();
-        ab.AMethod();
-        ab.BMethod();
+        //Delegate is a type safe function pointer
+        HelloFunctionDelegate del = new HelloFunctionDelegate(Hello);
+        del("Hello from delegates");
+    }
+
+    public static void Hello(string strMessage)
+    {
+        Console.WriteLine(strMessage);
     }
 }
