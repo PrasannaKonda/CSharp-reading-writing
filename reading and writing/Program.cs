8361985e6b109ee2f315c956a28be5860c66d79d
PrasannaKonda <kondalakshmiprasanna1804@gmail.com>
2025-08-29 10:46:52 +0530

    structures

--- reading and writing/Program.cs
+++ reading and writing/Program.cs
@@ -1,26 +1,57 @@
 ﻿using System;
 
-public class ParentClass
+public struct Customer
 {
-    public ParentClass()
+    private int _id;
+    private string _name;
+
+    public int Id
     {
-        Console.WriteLine("ParentClass Constructor Called");
+        get { return this._id; }
+        set { this._id = value; }
     }
-}
 
-public class ChildClass : ParentClass
-{
-    public ChildClass()
+    public string Name
+    {
+        get { return this._name; }
+        set { this._name = value; } 
+    }
+
+    public Customer()
     { 
-        Console.WriteLine("ChildClass Constructor Called");
+    
+    }
+
+    public Customer(int Id, string Name)
+    {
+        this._id = Id;
+        this._name = Name;
+    }
+
+    public void PrintDetails()
+    {
+        Console.WriteLine("ID = {0} && Name = {1}", this._id, this._name);
     }
 
 }
 
-public class Program 
+public class  Program
 {
     public static void Main()
-    { 
-        ChildClass child = new ChildClass();
+    {
+        Customer c1 = new Customer(101, "Lakshmi");
+        c1.PrintDetails();
+
+        Customer c2 = new Customer();
+        c2.Id = 102;
+        c2.Name = "Vishnu";
+        c2.PrintDetails();
+
+        Customer c3 = new Customer
+        {
+            Id = 103,
+            Name = "Jaswik"
+        };
+        c3.PrintDetails();
     }
 }
