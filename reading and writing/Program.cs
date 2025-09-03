6c10b3c7cddf95868d24554d569592a9ecfcf924
PrasannaKonda <kondalakshmiprasanna1804@gmail.com>
2025-09-03 09:58:26 +0530

    Example for delegate hardcode

--- reading and writing/Program.cs
+++ reading and writing/Program.cs
@@ -1,17 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 
-public delegate void HelloFunctionDelegate(string Message);
-public class Program
+class Program
 {
     public static void Main()
     {
-        //Delegate is a type safe function pointer
-        HelloFunctionDelegate del = new HelloFunctionDelegate(Hello);
-        del("Hello from delegates");
-    }
+        List<Employee> empList = new List<Employee>();
 
-    public static void Hello(string strMessage)
+        empList.Add(new Employee() { ID = 101, Name = "Mary", Salary = 5000, Experience = 5 });
+        empList.Add(new Employee() { ID = 102, Name = "Mike", Salary = 4000, Experience = 4 });
+        empList.Add(new Employee() { ID = 103, Name = "John", Salary = 6000, Experience = 6 });
+        empList.Add(new Employee() { ID = 104, Name = "Todd", Salary = 3000, Experience = 3 });
+
+        Employee.PromoteEmployee(empList);
+    }
+}
+
+class Employee
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public int Salary { get; set; }
+    public int Experience { get; set; }
+
+    public static void PromoteEmployee(List<Employee> empList)
     {
-        Console.WriteLine(strMessage);
+        foreach (Employee employee in empList)
+        {
+            if (employee.Experience >= 5)
+            {
+                Console.WriteLine(employee.Name + " promoted");
+            }
+        }
     }
 }
